7ccbebbfa8eaaa7ed820d37f98b2e76aeb8b0a06
Herbert Bergauer <Herbert.Bergauer@cern.ch>
2015-08-28 10:55:12 +0000

    updated for gtlv0.0.7 and fdl v0.0.13

--- mp7_ugt/firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
+++ mp7_ugt/firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
@@ -110,11 +110,11 @@ type ipb_regs_array is array (natural range <>) of std_logic_vector(31 downto 0)
 -- Revision 2 is based on fixed bug in muon_conditions.vhd.
     constant GTL_FW_MAJOR_VERSION      : integer range 0 to 255 := 0;
     constant GTL_FW_MINOR_VERSION      : integer range 0 to 255 := 0;
-    constant GTL_FW_REV_VERSION        : integer range 0 to 255 := 5;
+    constant GTL_FW_REV_VERSION        : integer range 0 to 255 := 7;
 -- FDL firmware version
     constant FDL_FW_MAJOR_VERSION      : integer range 0 to 255 := 0;
     constant FDL_FW_MINOR_VERSION      : integer range 0 to 255 := 0;
-    constant FDL_FW_REV_VERSION        : integer range 0 to 255 := 12;
+    constant FDL_FW_REV_VERSION        : integer range 0 to 255 := 13;
 -- ==================================================================================================
 
 constant BUNCHES_PER_ORBIT : natural range 3564 to 3564 := LHC_BUNCH_COUNT;

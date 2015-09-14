ecc706603ec643626503856cbcde2e913a651f98
jwittman <jwittman@cern.ch>
2015-09-14 11:01:45 +0000

    refs trac-1355 : changed area constraints http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1355.html

--- mp7_ugt/firmware/ucf/algo_constraints.xdc
+++ mp7_ugt/firmware/ucf/algo_constraints.xdc
@@ -1,2 +1,5 @@
 # Place constraints for the uGT algo block in this file:
-    
+
+# add_cells_to_pblock [get_pblock payload] payload
+# add_cells_to_pblock [get_pblock payload] [get_cells payload/frame_i]
+# add_cells_to_pblock [get_pblock payload] [get_cells payload/gtl_fdl_wrapper_i]
--- mp7_ugt/firmware/ucf/mp7_payload.tcl
+++ mp7_ugt/firmware/ucf/mp7_payload.tcl
@@ -1,13 +1,13 @@
 # Area constraints for MP7 null algo
 
-create_pblock payload
-resize_pblock [get_pblocks payload] -add {SLICE_X40Y0:SLICE_X181Y449}
-resize_pblock [get_pblocks payload] -add {RAMB18_X2Y0:RAMB18_X12Y179}
-resize_pblock [get_pblocks payload] -add {RAMB36_X2Y0:RAMB36_X12Y89}
-
-add_cells_to_pblock [get_pblock payload] payload
-add_cells_to_pblock [get_pblock payload] [get_cells payload/frame_i]
-add_cells_to_pblock [get_pblock payload] [get_cells payload/gtl_fdl_wrapper_i]
+# create_pblock payload
+# resize_pblock [get_pblocks payload] -add {SLICE_X40Y0:SLICE_X181Y449}
+# resize_pblock [get_pblocks payload] -add {RAMB18_X2Y0:RAMB18_X12Y179}
+# resize_pblock [get_pblocks payload] -add {RAMB36_X2Y0:RAMB36_X12Y89}
+#
+# add_cells_to_pblock [get_pblock payload] payload
+# add_cells_to_pblock [get_pblock payload] [get_cells payload/frame_i]
+# add_cells_to_pblock [get_pblock payload] [get_cells payload/gtl_fdl_wrapper_i]
 
 # area constraints for outputs
 create_pblock payload0
@@ -18,12 +18,14 @@ create_pblock payload2
 resize_pblock [get_pblocks payload2] -add {SLICE_X106Y300:SLICE_X180Y349}
 create_pblock payload3
 resize_pblock [get_pblocks payload3] -add {SLICE_X106Y250:SLICE_X180Y299}
+create_pblock payload4
+resize_pblock [get_pblocks payload4] -add {SLICE_X106Y200:SLICE_X180Y249}
 
 # Algo area constraints
-add_cells_to_pblock [get_pblocks payload0] [get_cells -quiet [list payload/frame_i/output_mux_i/mux0_i*]]
-add_cells_to_pblock [get_pblocks payload0] [get_cells -quiet [list payload/frame_i/output_mux_i/mux1_i*]]
-add_cells_to_pblock [get_pblocks payload0] [get_cells -quiet [list payload/frame_i/output_mux_i/mux2_i*]]
-add_cells_to_pblock [get_pblocks payload0] [get_cells -quiet [list payload/frame_i/output_mux_i/mux3_i*]]
+add_cells_to_pblock [get_pblocks payload4] [get_cells -quiet [list payload/frame_i/output_mux_i/mux0_i*]]
+add_cells_to_pblock [get_pblocks payload4] [get_cells -quiet [list payload/frame_i/output_mux_i/mux1_i*]]
+add_cells_to_pblock [get_pblocks payload4] [get_cells -quiet [list payload/frame_i/output_mux_i/mux2_i*]]
+add_cells_to_pblock [get_pblocks payload4] [get_cells -quiet [list payload/frame_i/output_mux_i/mux3_i*]]
 
 # add_cells_to_pblock [get_pblocks payload_1] [get_cells -quiet [list payload/frame_i/output_mux_i/mux4_i*]]
 # add_cells_to_pblock [get_pblocks payload_1] [get_cells -quiet [list payload/frame_i/output_mux_i/mux5_i*]]

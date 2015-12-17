f6a360e8c8e1f9e5b0f302f2dd1117d94663fc19
Herbert Bergauer <Herbert.Bergauer@cern.ch>
2015-12-17 14:35:56 +0000

    added parser for path of sim results file

--- mp7_ugt/firmware/sim/check_sum_results_4_algo_indices.py
+++ mp7_ugt/firmware/sim/check_sum_results_4_algo_indices.py
@@ -1,14 +1,22 @@
 #!/bin/env python
 
-path = "sim_results_gtl_fdl_wrapper_TestVector_L1Menu_Collisions2015_25nsStage1_v6_uGT_v2_TTbar"
+#path = "sim_results_gtl_fdl_wrapper_TestVector_L1Menu_Collisions2015_25nsStage1_v6_uGT_v2_TTbar"
+#path = "sim_results_gtl_fdl_wrapper_L1Menu_Correlation_2015_hb_test"
 
+import argparse
+
+## HB 2015-12-17: inserted parser for path of sim results file
+def parse():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help = "path to sim results file")
+    return parser.parse_args()
 
 def hex2bin(s):
   padding = len(s)*4
   decimal = int(s,16)
   binary = bin(decimal)[2:].zfill(padding)
   return binary
-
 
 def compare(array):
   if len(array) != 2:
@@ -27,6 +35,9 @@ def compare(array):
   if error:
     print " @ %s" % error
 
+# Parse command line arguments.
+args = parse()
+path = (args.path)
 
 data = {}
 key = ''

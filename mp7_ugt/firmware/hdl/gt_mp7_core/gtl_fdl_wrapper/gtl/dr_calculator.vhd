b00193484d3b44efd2c3b2a5ac1dac52c9a0ba26
Herbert Bergauer <Herbert.Bergauer@cern.ch>
2016-04-08 09:11:35 +0000

    refs trac-1355 : bug fixed in dr_calculator.vhd (upper_limit_vector and lower_limit_vector) http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1355.html

--- mp7_ugt/firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/dr_calculator.vhd
+++ mp7_ugt/firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/dr_calculator.vhd
@@ -40,31 +40,33 @@ entity dr_calculator is
     port(
         diff_eta : in std_logic_vector(DETA_DPHI_VECTOR_WIDTH-1 downto 0);
         diff_phi : in std_logic_vector(DETA_DPHI_VECTOR_WIDTH-1 downto 0);
-        dr_comp : out std_logic
+        dr_comp : out std_logic;
+        upper_limit_vector_sim : out std_logic_vector(DETA_DPHI_VECTOR_WIDTH*2-1 downto 0);
+        lower_limit_vector_sim : out std_logic_vector(DETA_DPHI_VECTOR_WIDTH*2-1 downto 0);
+        dr_squared_sim : out std_logic_vector(DETA_DPHI_VECTOR_WIDTH*2-1 downto 0)        
     );
 end dr_calculator;
 
 architecture rtl of dr_calculator is
--- -- HB 2015-11-26: length of std_logic_vector for Delta-R (dr_squared) and limits.
---     constant DR_VECTOR_WIDTH : positive := DETA_DPHI_VECTOR_WIDTH*2;
--- -- HB 2015-11-26: multiplication factor for limits (number of relevant position after decimal point - DR_PRECISION => 1, globaly set in gtl.pkg).
---     constant DR_PRECISION_FACTOR : real := real(10**DR_PRECISION);
--- -- HB 2015-11-26: multiplication factor for limits vectors. INV_MASS_COSH_COS_PRECISION: number of relevant position after decimal point for cosh_deta and cos_dphi, globaly set in gtl.pkg.
---     constant FACTOR_4_VECTOR : std_logic_vector((INV_MASS_COSH_COS_PRECISION+1)*4-1 downto 0) := conv_std_logic_vector(10**(INV_MASS_COSH_COS_PRECISION+1),(INV_MASS_COSH_COS_PRECISION+1)*4);
-
     signal dr_squared : std_logic_vector(DETA_DPHI_VECTOR_WIDTH*2-1 downto 0);
     signal upper_limit_vector : std_logic_vector(DETA_DPHI_VECTOR_WIDTH*2-1 downto 0);
     signal lower_limit_vector : std_logic_vector(DETA_DPHI_VECTOR_WIDTH*2-1 downto 0);
 begin
 
 -- HB 2015-11-26: converting limits to std_logic_vector for comparison.
-    upper_limit_vector <= conv_std_logic_vector(integer(upper_limit*real(10**DETA_DPHI_PRECISION)),DETA_DPHI_VECTOR_WIDTH*2);
-    lower_limit_vector <= conv_std_logic_vector(integer(lower_limit*real(10**DETA_DPHI_PRECISION)),DETA_DPHI_VECTOR_WIDTH*2);
+-- HB 2016-04-08: bug fixed with DETA_DPHI_PRECISION*2.
+    upper_limit_vector <= conv_std_logic_vector(integer(upper_limit*real(10**(DETA_DPHI_PRECISION*2))),DETA_DPHI_VECTOR_WIDTH*2);
+    lower_limit_vector <= conv_std_logic_vector(integer(lower_limit*real(10**(DETA_DPHI_PRECISION*2))),DETA_DPHI_VECTOR_WIDTH*2);
 
 -- HB 2015-11-26: calculation of ΔR**2 with formular ΔR**2 = (eta1-eta2)**2+(phi1-phi2)**2
     dr_squared <= diff_eta*diff_eta+diff_phi*diff_phi;
 
 -- HB 2015-11-26: comparison
     dr_comp <= '1' when (dr_squared >= lower_limit_vector and dr_squared <= upper_limit_vector) else '0';
+    
+-- HB 2016-04-08: inserted outputs for waveform simulation.
+    upper_limit_vector_sim <= upper_limit_vector;
+    lower_limit_vector_sim <= lower_limit_vector;
+    dr_squared_sim <= dr_squared;
     
 end architecture rtl;

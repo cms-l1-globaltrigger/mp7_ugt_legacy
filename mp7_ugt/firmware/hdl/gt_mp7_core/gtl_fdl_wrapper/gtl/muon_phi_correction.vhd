371cfdaa47efd65ce56076628b5a455c0ce3f0a7
Herbert Bergauer <Herbert.Bergauer@cern.ch>
2016-09-13 13:22:00 +0000

    refs trac-1355 : added and updated files for muon_phi_correction http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1355.html

--- mp7_ugt/firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/muon_phi_correction.vhd
+++ mp7_ugt/firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/muon_phi_correction.vhd
@@ -7,54 +7,20 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all; -- for function "CONV_INTEGER"
 
+use work.gtl_pkg.all;
+use work.muon_phi_correction_pkg.all;
+
 entity muon_phi_correction is
     port(
-        pt : in std_logic_vector(9-1 downto 0);
-        phi : in std_logic_vector(10-1 downto 0);
-        charge : in std_logic_vector(2-1 downto 0);
-        phi_o : out std_logic_vector(10-1 downto 0);
+        pt : in std_logic_vector(D_S_I_MUON.pt_high-D_S_I_MUON.pt_low downto 0);
+        phi : in std_logic_vector(D_S_I_MUON.phi_high-D_S_I_MUON.phi_low downto 0);
+        charge : in std_logic_vector(D_S_I_MUON.charge_high-D_S_I_MUON.charge_low downto 0);
+        phi_o : out std_logic_vector(D_S_I_MUON.phi_high-D_S_I_MUON.phi_low downto 0);
         phi_temp_o : out integer
     );
 end muon_phi_correction;
 
 architecture rtl of muon_phi_correction is
-
--- HB 2016-08-17: LUT was generated center values of pt and the formular: 2.677/pt**1.014 (pt [GeV] center value of bin). LUT values in bins.
-type phi_correction_lut_array is array (0 to 2**9-1) of natural range 0 to 425;
-constant PHI_CORRECTION_LUT : phi_correction_lut_array := (
-0,	425,	329,	196,	139,	108,	88,	74,	64,	57,	51,	46,	42,	38,	35,	33,
-31,	29,	27,	26,	24,	23,	22,	21,	20,	19,	19,	18,	17,	17,	16,	15,
-15,	15,	14,	14,	13,	13,	13,	12,	12,	12,	11,	11,	11,	11,	10,	10,
-10,	10,	9,	9,	9,	9,	9,	9,	8,	8,	8,	8,	8,	8,	8,	7,
-7,	7,	7,	7,	7,	7,	7,	7,	7,	6,	6,	6,	6,	6,	6,	6,
-6,	6,	6,	6,	6,	6,	5,	5,	5,	5,	5,	5,	5,	5,	5,	5,
-5,	5,	5,	5,	5,	5,	5,	5,	4,	4,	4,	4,	4,	4,	4,	4,
-4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,	4,
-4,	4,	4,	4,	4,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,
-3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,
-3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,	3,
-3,	3,	3,	3,	3,	3,	3,	3,	3,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,
-2,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,
-1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1
-);
 
     signal phi_value : natural;
     signal phi_correction : natural;

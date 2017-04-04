30b6b2eda5e377ea25ba25ec20c13fe6a1b89744
jwittman <jwittman@cern.ch>
2017-04-04 09:16:59 +0000

    refs trac-1350: updated fw_generation script http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html

--- mp7_ugt/scripts/ugt_mp7_fw_generation.sh
+++ mp7_ugt/scripts/ugt_mp7_fw_generation.sh
@@ -5,27 +5,33 @@ set -e
 ## Description:
 ## runs TME, VHDL Producer, creates project for FW synthesis, executes "make project" and starts screen for synthesis
 
-nr_arguments=8
+nr_arguments=9
 max_nr_modules=3
+
+function error_exit
+{
+	echo "$@" 1>&2
+	exit 1
+}
 
 if [ "$1" == "-h" -o "$1" == "--help" ]
 then
-    echo 
+    echo
     echo ========================
     echo List of $nr_arguments arguments: Xml4Tme PathUtm NumberModules PathL1MenuDir PathFw MP7FwTag UserName FWVersion
-    echo 
+    echo
     echo Description of $nr_arguments arguments:
-    echo 
+    echo
     echo Xml4Tme: location of xml file for TME
     echo PathUtm: location of utm directory [for executing tmVhdlProducer]
     echo NumberModules: number of ugt MP7 modules [used for this FW version]
     echo PathL1MenuDir: location of L1Menu directory
     echo PathFw: location of mp7_ugt directory [for making FW project]
-    echo MP7FwTag: [currently: mp7fw_v2_2_0]
+    echo MP7FwTag: [currently: mp7fw_v2_2_1]
     echo UserName: CERN user name
     echo FWVersion: FW version [e.g.: 10a2]
     echo ========================
-    echo 
+    echo
     exit 0
 else
     if [[ $# -ne $nr_arguments ]] ; then
@@ -51,37 +57,35 @@ else
     l1_menu_name=$(sed -n '/<name>L1Menu/p' $1 | cut -d'>' -f2 | cut -d'<' -f1)
     cd
 ## HB 2016-12-01: used a temporary file as output of TME for VHDL Producer
-    echo ========================
-    echo INFO: TME is checking $1 and exporting a temporary file [~/$l1_menu_name\_temp.xml] for VHDL Producer
-    echo ========================
-    tm-editor $1 --export-xml ~/$l1_menu_name\_temp.xml
-    cd $2
-    source setup.sh
-    cd $2/tmVhdlProducer
-    tm-vhdlproducer ~/$l1_menu_name\_temp.xml --output=$4 --modules $3
-    cd
-    echo ========================
-    echo INFO: removed ~/$l1_menu_name\_temp.xml
-    echo ========================
-    rm $l1_menu_name\_temp.xml
-    cd $5/scripts
-    python makeProject.py -t $6 -u $7 -b 0x$8 -p ~/work/mp7fwdir/ -m $4/$l1_menu_name\_m$3
-## HB 2017-03-08: switched to Vivado 2016.4
-#    source /opt/xilinx/Vivado/2016.2/settings64.sh
-    source /opt/xilinx/Vivado/2016.4/settings64.sh
+    if [ "$9" = "1" ]; then
+								echo ========================
+						    echo INFO: TME is checking $1 and exporting a temporary file [~/$l1_menu_name\_temp.xml] for VHDL Producer
+						    echo ========================
+						    tm-editor $1 --export-xml ~/$l1_menu_name\_temp.xml || error_exit "Could not open tm-editor! Aborting"
+						    cd $2 || error_exit "Cannot change directory! Aborting"
+						    source setup.sh || error_exit "Could not source setup.sh! Aborting"
+						    cd $2/tmVhdlProducer || error_exit "Cannot change directory! Aborting"
+						    tm-vhdlproducer ~/$l1_menu_name\_temp.xml --output=$4 --modules $3 || error_exit "VHDL producer failed! Aborting"
+						    cd
+						    echo ========================
+						    echo INFO: removed ~/$l1_menu_name\_temp.xml
+						    echo ========================
+						    rm $l1_menu_name\_temp.xml || error_exit "Cannot remove temp XML! Aborting"
+						    cd $5/scripts || error_exit "Cannot change directory! Aborting"
+						    python makeProject.py -t $6 -u $7 -b 0x$8 -p ~/work/mp7fwdir/ -m $4/$l1_menu_name\_m$3 || error_exit "makePropject.py script failed! Aborting"
+    else
+								cd $5/scripts || error_exit "Cannot change directory! Aborting"
+			          python makeProject.py -t $6 -u $7 -b 0x$8 -p ~/work/mp7fwdir/ -m $1 || error_exit "makePropject.py script failed! Aborting"
+		fi
     nr_modules=0
     while [ $nr_modules -lt $3 ]; do
-	cd ~/work/mp7fwdir/$6/build_0x$8/$l1_menu_name\_m$3/module_$nr_modules
-	make project
 ## HB 2016-12-01: starting synthesis in screen
-	screen -S $8_$nr_modules -dm make bitfile
+	screen -dmS $8_$nr_modules bash -c "source /opt/xilinx/Vivado/2016.4/settings64.sh; cd ~/work/mp7fwdir/$6/build_0x$8/$l1_menu_name\_m$3/module_$nr_modules; make project && make bitfile" || error_exit "Failed opening screen! Aborting"
 	nr_modules=$((nr_modules + 1))
     done
     echo ========================
     echo INFO: synthesis for $nr_modules modules of FW 0x$8 started [screen]
     echo ========================
-    screen -r
+    screen -ls
     exit 0
 fi
-
-

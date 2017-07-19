8b894fcd83a34d0b0c4a19d6070d2d1aa9e1edfe
jwittman <jwittman@cern.ch>
2017-07-19 09:59:07 +0000

    refs trac-2291: added address tables for AMC502 finOR preview board http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/2291.html

--- amc502_extcond/firmware/hdl/user_package.vhd
+++
@@ -1,9 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-
-package user_package is
--- type definition for unconstraint range of IPbus register blocks
-    type ipb_regs_array is array (natural range <>) of std_logic_vector(31 downto 0);
-
-    constant RST_ACT : std_logic := '1';
-end user_package;

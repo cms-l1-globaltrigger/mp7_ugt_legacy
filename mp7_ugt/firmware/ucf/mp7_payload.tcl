efc7ccd35e77a4d49f87375a1feab6916aec150c
Herbert Bergauer <Herbert.Bergauer@cern.ch>
2017-12-14 12:43:49 +0000

    refs trac-1355 : inserted constraints for DSPs in payload http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1355.html

--- mp7_ugt/firmware/ucf/mp7_payload.tcl
+++ mp7_ugt/firmware/ucf/mp7_payload.tcl
@@ -3,24 +3,37 @@
 ## HB 2016-08-31: ../ucf/algo_constraints.xdc not used anymore (see uGT_algo.dep)
 ## JW 2017-10-06: Added payload area constraints
 
+# HB 2017-10-13: Extended payload region to former left quad regions(quad_9 to quad_17)
 # JW 2017-10-06: Extended payload region to use space of unused GTHs (Greg's suggestion)
 resize_pblock [get_pblocks payload] -add {SLICE_X0Y0:SLICE_X29Y449}
 resize_pblock [get_pblocks payload] -add {RAMB18_X0Y0:RAMB18_X1Y179}
 resize_pblock [get_pblocks payload] -add {RAMB36_X0Y0:RAMB36_X1Y89}
+# HB 2017-10-13: DSPs into payload region
+resize_pblock [get_pblocks payload] -add {DSP48_X0Y0:DSP48_X1Y179}
 
+# HB 2017-10-13: Extended payload region to former right bottom quad regions (quad_7 and quad_8)
 resize_pblock [get_pblocks payload] -add {SLICE_X192Y0:SLICE_X221Y99}
 resize_pblock [get_pblocks payload] -add {RAMB18_X13Y0:RAMB18_X14Y39}
 resize_pblock [get_pblocks payload] -add {RAMB36_X13Y0:RAMB36_X14Y19}
+# HB 2017-10-13: DSPs into payload region
+resize_pblock [get_pblocks payload] -add {DSP48_X17Y0:DSP48_X17Y39}
 
-# JW 2017-10-06: Extend payload region to unused parts of the ctrl region (Greg's suggestion)
+# HB 2017-10-13: Resize ctrl region
 resize_pblock [get_pblocks ctrl] -remove {CLOCKREGION_X0Y9:CLOCKREGION_X1Y9}
 resize_pblock [get_pblocks ctrl] -add {SLICE_X74Y450:SLICE_X221Y499}
 resize_pblock [get_pblocks ctrl] -add {RAMB18_X4Y180:RAMB18_X14Y199}
 resize_pblock [get_pblocks ctrl] -add {RAMB36_X4Y90:RAMB36_X14Y99}
 
+# HB 2017-10-13: Extended payload region to former left top region of ctrl region
+# JW 2017-10-06: Extend payload region to unused parts of the ctrl region (Greg's suggestion)
 resize_pblock [get_pblocks payload] -add {SLICE_X0Y450:SLICE_X73Y499}
 resize_pblock [get_pblocks payload] -add {RAMB18_X0Y180:RAMB18_X3Y199}
 resize_pblock [get_pblocks payload] -add {RAMB36_X0Y90:RAMB36_X3Y99}
+# HB 2017-10-13: DSPs into payload region
+resize_pblock [get_pblocks payload] -add {DSP48_X0Y180:DSP48_X3Y199}
+
+# HB 2017-10-13: DSPs into payload region (payload part defined in area_constraints.tcl of mp7)
+resize_pblock [get_pblocks payload] -add {DSP48_X2Y0:DSP48_X16Y179}
 
 # HB 2017-10-06: used pblock payload for payload.vhd
 add_cells_to_pblock [get_pblock payload] payload

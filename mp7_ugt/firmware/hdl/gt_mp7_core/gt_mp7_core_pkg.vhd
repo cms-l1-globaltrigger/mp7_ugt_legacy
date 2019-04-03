87022c2e0953ffea35cea650d66398d9319c562d
herbberg <Herbert.Bergauer@oeaw.ac.at>
2019-04-03 13:28:47 +0200

    updated FRAME_VERSION number as a mp7_ugt firmware release number

--- mp7_ugt/firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
+++ mp7_ugt/firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
@@ -1,6 +1,7 @@
 
 -- actual versions:
---	frame: v1.2.3
+-- use "FRAME_VERSION" as mp7_ugt release fw version (used for tag name, too)
+--	mp7_ugt (=frame): v1.3.0
 --	gtl: v1.6.0
 --	fdl: v1.2.2
 
@@ -34,8 +35,8 @@ package gt_mp7_core_pkg is
 -- ==================================================================================================
 -- FRAME version (given by the editor of frame.vhd)
     constant FRAME_MAJOR_VERSION      : integer range 0 to 255 := 1;
-    constant FRAME_MINOR_VERSION      : integer range 0 to 255 := 2;
-    constant FRAME_REV_VERSION        : integer range 0 to 255 := 3;
+    constant FRAME_MINOR_VERSION      : integer range 0 to 255 := 3;
+    constant FRAME_REV_VERSION        : integer range 0 to 255 := 0;
 	constant FRAME_VERSION : std_logic_vector(31 downto 0) := X"00" &
            std_logic_vector(to_unsigned(FRAME_MAJOR_VERSION, 8)) &
            std_logic_vector(to_unsigned(FRAME_MINOR_VERSION, 8)) &

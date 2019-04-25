1e62f369187d506521afd12210f592cea6d7d58d
herbberg <Herbert.Bergauer@oeaw.ac.at>
2019-04-25 11:08:37 +0200

    updated for correct use of l1menu_files.tcl content

--- mp7_ugt/firmware/cfg/l1menu_files.tcl
+++ mp7_ugt/firmware/cfg/l1menu_files.tcl
@@ -1,13 +1,15 @@
 #!/usr/bin/tclsh
 
 set mod_id $env(module_id)
-puts $mod_id
+# puts $mod_id
 
 set gtl_pkg "../../src/module_${mod_id}/gtl_pkg.vhd"
-puts $gtl_pkg
+# puts $gtl_pkg
 set algo_mapping_rop "../../src/module_${mod_id}/algo_mapping_rop.vhd"
-puts $algo_mapping_rop
+# puts $algo_mapping_rop
 set gtl_module "../../src/module_${mod_id}/gtl_module.vhd"
-puts $gtl_module
+# puts $gtl_module
 
-add_files -norecurse -fileset sources_1 $gtl_pkg $algo_mapping_rop $gtl_module
+add_files -norecurse -fileset sources_1 $gtl_pkg
+add_files -norecurse -fileset sources_1 $algo_mapping_rop
+add_files -norecurse -fileset sources_1 $gtl_module

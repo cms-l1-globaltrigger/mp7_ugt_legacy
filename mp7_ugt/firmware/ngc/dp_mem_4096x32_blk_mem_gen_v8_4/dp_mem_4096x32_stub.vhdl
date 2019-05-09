9a0ba24f7a7f8e17a08a83ce9ddfca7b8176d613
herbberg <Herbert.Bergauer@oeaw.ac.at>
2019-05-09 14:38:08 +0200

    added directory in ngc

---
+++ mp7_ugt/firmware/ngc/dp_mem_4096x32_blk_mem_gen_v8_4/dp_mem_4096x32_stub.vhdl
@@ -0,0 +1,41 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+-- Date        : Tue May  7 13:41:02 2019
+-- Host        : ugt-synth running 64-bit Ubuntu 16.04.5 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/bergauer/work_ipbb/tests/mp7_ugt/mp7fw_v2_4_1/L1Menu_Collisions2018_v2_1_0-d98/0xdddf/proj/mp7_ugt_0xdddf_0/top/top.srcs/sources_1/ip/dp_mem_4096x32/dp_mem_4096x32_stub.vhdl
+-- Design      : dp_mem_4096x32
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7vx690tffg1927-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity dp_mem_4096x32 is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 11 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    clkb : in STD_LOGIC;
+    enb : in STD_LOGIC;
+    web : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addrb : in STD_LOGIC_VECTOR ( 11 downto 0 );
+    dinb : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end dp_mem_4096x32;
+
+architecture stub of dp_mem_4096x32 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[0:0],addra[11:0],dina[31:0],douta[31:0],clkb,enb,web[0:0],addrb[11:0],dinb[31:0],doutb[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_1,Vivado 2018.2";
+begin
+end;

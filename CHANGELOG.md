b53f9b507d3205c64713cf9afa5320f7954516bf
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-10-10 14:48:28 +0200

    updated firmware release

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [mp7_ugt_v1_9_1] - 2019-10-10
+### Comment
+
+- mp7_ugt firmware release v1.9.1 is based on frame v1.2.3, gtl v1.8.0 and fdl v1.3.0.
+- this tag was created after relocation of project files
+
--- firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
+++ firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
@@ -1,7 +1,7 @@
 
 -- actual versions:
 -- use "FRAME_VERSION" as mp7_ugt release fw version (used for tag name)
--- mp7_ugt (=FRAME_VERSION): v1.9.0
+-- mp7_ugt (=FRAME_VERSION): v1.9.1
 --
 -- frame: v1.2.3 (see frame.vhd)
 -- gtl: v1.8.0 (see gtl_module_tpl.vhd)
@@ -38,7 +38,7 @@ package gt_mp7_core_pkg is
 -- FRAME version (given by the editor of frame.vhd)
     constant FRAME_MAJOR_VERSION      : integer range 0 to 255 := 1;
     constant FRAME_MINOR_VERSION      : integer range 0 to 255 := 9;
-    constant FRAME_REV_VERSION        : integer range 0 to 255 := 0;
+    constant FRAME_REV_VERSION        : integer range 0 to 255 := 1;
 	constant FRAME_VERSION : std_logic_vector(31 downto 0) := X"00" &
            std_logic_vector(to_unsigned(FRAME_MAJOR_VERSION, 8)) &
            std_logic_vector(to_unsigned(FRAME_MINOR_VERSION, 8)) &

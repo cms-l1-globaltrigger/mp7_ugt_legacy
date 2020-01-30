51b2a6b320381183a91ff1afd89047597f4991f9
Herbert Bergauer <Herbert.Bergauer@oeaw.ac.at>
2020-01-30 10:26:49 +0100

    Update gt_mp7_core_pkg.vhd

--- firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
+++ firmware/hdl/gt_mp7_core/gt_mp7_core_pkg.vhd
@@ -1,11 +1,14 @@
 
 -- actual versions:
 -- use "FRAME_VERSION" as mp7_ugt release fw version (used for tag name)
--- mp7_ugt (=FRAME_VERSION): v1.10.6
+-- mp7_ugt (=FRAME_VERSION): v1.10.7
 --
 -- frame: v1.2.3 (see frame.vhd)
--- gtl: v1.9.2 (see gtl_module_tpl.vhd)
+-- gtl: v1.9.3 (see gtl_module_tpl.vhd)
 -- fdl: v1.3.6 (see fdl_module.vhd)
+
+-- gtl history:
+-- HB 2020-01-30: v1.9.3: cleaned up code in esums_comparators.vhd and min_bias_hf_conditions.vhd.
 
 -- [HB 2016-10-11: mp7_ugt v1.9.3 - for correct tag (typo in tag name in CHANGELOG.md)]
 -- HB 2016-09-19: removed more unused constants
@@ -39,7 +42,7 @@ package gt_mp7_core_pkg is
 -- FRAME version (given by the editor of frame.vhd)
     constant FRAME_MAJOR_VERSION      : integer range 0 to 255 := 1;
     constant FRAME_MINOR_VERSION      : integer range 0 to 255 := 10;
-    constant FRAME_REV_VERSION        : integer range 0 to 255 := 6;
+    constant FRAME_REV_VERSION        : integer range 0 to 255 := 7;
 	constant FRAME_VERSION : std_logic_vector(31 downto 0) := X"00" &
            std_logic_vector(to_unsigned(FRAME_MAJOR_VERSION, 8)) &
            std_logic_vector(to_unsigned(FRAME_MINOR_VERSION, 8)) &
@@ -47,7 +50,7 @@ package gt_mp7_core_pkg is
 -- GTL firmware (fix part) version
     constant GTL_FW_MAJOR_VERSION      : integer range 0 to 255 := 1;
     constant GTL_FW_MINOR_VERSION      : integer range 0 to 255 := 9;
-    constant GTL_FW_REV_VERSION        : integer range 0 to 255 := 2;
+    constant GTL_FW_REV_VERSION        : integer range 0 to 255 := 3;
 -- FDL firmware version
     constant FDL_FW_MAJOR_VERSION      : integer range 0 to 255 := 1;
     constant FDL_FW_MINOR_VERSION      : integer range 0 to 255 := 3;

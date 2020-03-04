d19e4fccf4bf5066b8cbd6bafb87d3fefe57a680
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-03-04 16:37:16 +0100

    added attribute use_dsp

--- firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/sum_mass_calc.vhd
+++ firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/sum_mass_calc.vhd
@@ -2,6 +2,7 @@
 -- Calculation of sum mass of 3 objects.
 
 -- Version history:
+-- HB 2020-03-04: Added attribute use_dsp.
 -- HB 2020-02-18: First design.
 
 library ieee;
@@ -24,11 +25,15 @@ end sum_mass_calc;
 
 architecture rtl of sum_mass_calc is
 
-    signal sum_mass_temp : std_logic_vector(DATA_WIDTH downto 0);
+    signal sum_mass_1_2 : std_logic_vector(DATA_WIDTH downto 0);
     
+    attribute use_dsp : string;
+    attribute use_dsp of sum_mass_1_2 : signal is "yes";
+    attribute use_dsp of sum_mass : signal is "yes";
+
 begin
 
-    sum_mass_temp <= ('0' & in1) + in2;
-    sum_mass <= ('0' & sum_mass_temp) + in3;
+    sum_mass_1_2 <= ('0' & in1) + in2;
+    sum_mass <= ('0' & sum_mass_1_2) + in3;
           
 end architecture rtl;

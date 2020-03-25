95b405199afe9411056ddfb1012004b07615e47f
herbberg <Herbert.Bergauer@oeaw.ac.at>
2020-03-25 16:27:21 +0100

    updates for simulation

--- firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/invmass_div_dr_calculator.vhd
+++ firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/invmass_div_dr_calculator.vhd
@@ -10,47 +10,69 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.fixed_pkg.all;
 
+use work.math_pkg.all;
 use work.gtl_pkg.all;
 
 entity invmass_div_dr_calculator is
     generic (
+        deta_int_digits: positive;
+        dphi_int_digits: positive;
+        pt_int_digits: positive;
+        cosh_deta_int_digits: positive;
 -- limits for comparison of invariant mass divided by deltaR
-        mass_div_dr_upper_limit: sfixed(36 downto -1);
-        mass_div_dr_lower_limit: sfixed(36 downto -1)
+        mass_div_dr_upper_limit: sfixed;
+        mass_div_dr_lower_limit: sfixed;
+        fract_digits: positive := 20
     );
     port(
-        diff_eta : in sfixed(5 downto -19);
-        diff_phi : in sfixed(4 downto -19);
-        pt1 : in sfixed(9 downto -17);
-        pt2 : in sfixed(9 downto -17);
-        cosh_deta : in sfixed(15 downto -19);
-        cos_dphi : in sfixed(2 downto -19);
+        diff_eta : in sfixed(deta_int_digits downto -fract_digits);
+        diff_phi : in sfixed(dphi_int_digits downto -fract_digits);
+        pt1 : in sfixed(pt_int_digits downto -fract_digits);
+        pt2 : in sfixed(pt_int_digits downto -fract_digits);
+        cosh_deta : in sfixed(cosh_deta_int_digits downto -fract_digits);
+        cos_dphi : in sfixed(1 downto -fract_digits);
         mass_div_dr_comp : out std_logic
     );
 end invmass_div_dr_calculator;
 
 architecture rtl of invmass_div_dr_calculator is
 
-    signal dr_sq : sfixed(10 downto -12);
---     signal dr_sq_off : sfixed(10 downto -12);
---     signal offset : sfixed(10 downto -12) := "000"&X"00001";
-    signal invariant_mass_sq_div2 : sfixed(36 downto -12);
-    signal invmass_sq_div2_div_dr_sq : sfixed(58 downto -12);
+    constant dr_sq_int_digits : positive := max(deta_int_digits*2, dphi_int_digits*2)+2;
+    constant inv_mass_int_digits : positive := pt_int_digits*2+cosh_deta_int_digits+3;
+    constant inv_mass_div_dr_int_digits : positive := inv_mass_int_digits+fract_digits+1;
+    
+    constant zero : sfixed(dr_sq_int_digits downto -fract_digits*2) := (others => '0');
+    signal dr_sq_temp : sfixed(dr_sq_int_digits downto -fract_digits*2);
+    signal dr_sq : sfixed(dr_sq_int_digits downto -fract_digits);
+    signal invariant_mass_sq_div2_temp : sfixed(inv_mass_int_digits downto -fract_digits*3);
+    signal invariant_mass_sq_div2 : sfixed(inv_mass_int_digits downto -fract_digits);
+    signal invmass_sq_div2_div_dr_sq_temp : sfixed(inv_mass_div_dr_int_digits downto -fract_digits-dr_sq_int_digits);
+    signal invmass_sq_div2_div_dr_sq : sfixed(inv_mass_div_dr_int_digits downto -fract_digits) := (others => '0');
 
     attribute use_dsp : string;
-    attribute use_dsp of dr_squared : signal is "yes";
+    attribute use_dsp of dr_sq : signal is "yes";
     attribute use_dsp of invariant_mass_sq_div2 : signal is "yes";
     attribute use_dsp of invmass_sq_div2_div_dr_sq : signal is "yes";
 
 begin
 
     
-    dr_sq <= diff_eta*diff_eta+diff_phi*diff_phi;
+    dr_sq_temp <= diff_eta*diff_eta+diff_phi*diff_phi;
+    dr_sq <= dr_sq_temp(dr_sq_int_digits downto -fract_digits);
     
-    invariant_mass_sq_div2 <= pt1 * pt2 * (cosh_deta - cos_dphi);
+    invariant_mass_sq_div2_temp <= pt1 * pt2 * (cosh_deta - cos_dphi);
+    invariant_mass_sq_div2 <= invariant_mass_sq_div2_temp(inv_mass_int_digits downto -fract_digits);
         
-    invmass_sq_div2_div_dr_sq <= invariant_mass_sq_div2 / dr_sq;
-        
-    mass_div_dr_comp <= '1' when invmass_sq_div2_div_dr_sq(36 downto -12) >= mass_div_dr_lower_limit and invmass_sq_div2_div_dr_sq(36 downto -12) <= mass_div_dr_upper_limit else '0';
+    div_zero_p: process(dr_sq)
+    begin
+        if dr_sq /= zero then
+            invmass_sq_div2_div_dr_sq_temp <= invariant_mass_sq_div2 / dr_sq;
+            invmass_sq_div2_div_dr_sq <= invmass_sq_div2_div_dr_sq_temp(inv_mass_div_dr_int_digits downto -fract_digits);
+        else
+            invmass_sq_div2_div_dr_sq(inv_mass_int_digits downto -fract_digits) <= invariant_mass_sq_div2;
+        end if;
+    end process div_zero_p;
+
+    mass_div_dr_comp <= '1' when invmass_sq_div2_div_dr_sq >= mass_div_dr_lower_limit and invmass_sq_div2_div_dr_sq <= mass_div_dr_upper_limit else '0';
         
 end architecture rtl;
--- firmware/sim/scripts/invmass_div_dr_calculator_wave.do
+++ firmware/sim/scripts/invmass_div_dr_calculator_wave.do
@@ -1,36 +1,24 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
-add wave -noupdate /invmass_div_dr_calculator_tb/eg_data
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/pt1
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/pt2
+add wave -noupdate -radix hexadecimal -radixshowbase 0 /invmass_div_dr_calculator_tb/eg_data
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/pt1
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/pt2
 add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/eg_eta_integer
 add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/eg_phi_integer
 add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/diff_eg_eg_eta_integer
 add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/diff_eg_eg_phi_integer
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/diff_eta
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/diff_phi
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/cosh_deta
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/cos_dphi
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/pt1_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/pt2_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/cosh_deta_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/cos_dphi_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/diff_eta_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/diff_phi_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/pt1_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/pt2_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/cosh_deta_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/cos_dphi_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/diff_eta_real
-add wave -noupdate /invmass_div_dr_calculator_tb/dut/diff_phi_real
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/dut/dr_squared
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/dut/invariant_mass_sq_div2
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/dut/invmass_sq_div2_div_dr_sq
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/dut/sim_mass_div_dr_lower_limit
-add wave -noupdate -radix decimal -radixshowbase 0 /invmass_div_dr_calculator_tb/dut/sim_mass_div_dr_upper_limit
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/diff_eta
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/diff_phi
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/cosh_deta
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/cos_dphi
+add wave -noupdate -radix sfixed /invmass_div_dr_calculator_tb/dut/mass_div_dr_upper_limit
+add wave -noupdate -radix sfixed /invmass_div_dr_calculator_tb/dut/mass_div_dr_lower_limit
+add wave -noupdate -radix sfixed /invmass_div_dr_calculator_tb/dut/dr_sq
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/dut/invariant_mass_sq_div2
+add wave -noupdate -radix sfixed -radixshowbase 0 /invmass_div_dr_calculator_tb/dut/invmass_sq_div2_div_dr_sq
 add wave -noupdate /invmass_div_dr_calculator_tb/dut/mass_div_dr_comp
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {117976 ps} 0}
+WaveRestoreCursors {{Cursor 1} {149496 ps} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 431
 configure wave -valuecolwidth 184
@@ -46,4 +34,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {0 ps} {708416 ps}
+WaveRestoreZoom {0 ps} {354208 ps}

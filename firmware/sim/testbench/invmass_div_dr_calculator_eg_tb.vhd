dd1868785cac4612584075aecf0460483441472d
herbberg <herbert.bergauer@oeaw.ac.at>
2020-04-22 10:56:25 +0200

    updated for tests with inverse deltaR value LUT

--- firmware/sim/testbench/invmass_div_dr_calculator_eg_tb.vhd
+++ firmware/sim/testbench/invmass_div_dr_calculator_eg_tb.vhd
@@ -17,50 +17,29 @@ use std.textio.all;
 
 use work.math_pkg.all;
 use work.gtl_pkg.all;
-use work.ufixed_luts_pkg.all;
 
 entity invmass_div_dr_calculator_eg_TB is
 end invmass_div_dr_calculator_eg_TB;
 
 architecture beh of invmass_div_dr_calculator_eg_TB is
 
-    constant deta_dphi_prec : positive := EG_EG_DETA_DPHI_PRECISION;
-    constant cosh_cos_prec : positive := EG_EG_COSH_COS_PRECISION;
-    constant pt_prec : positive := EG_PT_PRECISION;
-
-    constant deta_int_digits: positive := EG_DETA_INT_DIGITS;
-    constant dphi_int_digits: positive := EG_DPHI_INT_DIGITS;
-    constant pt_int_digits: positive := EG_PT_INT_DIGITS;
-    constant cosh_deta_int_digits: positive := EG_COSH_DETA_INT_DIGITS;
-    constant fract_digits: positive := FRACT_DIGITS_ALL;
-    
-    constant dr_sq_int_digits : positive := max(deta_int_digits*2, dphi_int_digits*2)+2; -- = 8
-    constant inv_mass_int_digits : positive := pt_int_digits*2+cosh_deta_int_digits+3; -- = 26
-    constant inv_mass_div_dr_int_digits : positive := inv_mass_int_digits+fract_digits+1; -- = 47
-    
-    constant mass_upper_limit: real := 18.9;
-    constant mass_lower_limit: real := 18.0;
+    constant mass_upper_limit: std_logic_vector(MAX_WIDTH_MASS_DIV_DR_LIMIT_VECTOR-1 downto 0) := X"0000_0000_8000_0000_0000";
+    constant mass_lower_limit: std_logic_vector(MAX_WIDTH_MASS_DIV_DR_LIMIT_VECTOR-1 downto 0) := X"0000_0000_0000_0000_0000";
     
     constant LHC_CLK_PERIOD  : time :=  25 ns;
 
-    signal jet_data : calo_objects_array(1 downto 0) := (X"00000000", X"00000000");
-    type pt_array is array (1 downto 0) of std_logic_vector(EG_PT_HIGH-EG_PT_LOW downto 0);
-    type phi_array is array (1 downto 0) of std_logic_vector(EG_PHI_HIGH-EG_PHI_LOW downto 0);
-    type eta_array is array (1 downto 0) of std_logic_vector(EG_ETA_HIGH-EG_ETA_HIGH downto 0);
-    signal pt: pt_array;
-    signal phi: phi_array;
-    signal eta: eta_array;
-    signal jet_eta_integer: diff_integer_inputs_array(0 to 1) := (others => 0);
-    signal jet_phi_integer: diff_integer_inputs_array(0 to 1) := (others => 0);
-    signal diff_jet_jet_eta_integer: dim2_max_eta_range_array(0 to 1, 0 to 1) := (others => (others => 0));
-    signal diff_jet_jet_phi_integer: dim2_max_phi_range_array(0 to 1, 0 to 1) := (others => (others => 0));
+    signal eg_data : calo_objects_array(1 downto 0) := (X"00000000", X"00000000");
 
-    signal diff_eta_int : natural range 0 to 9962;
-    signal diff_phi_int : natural range 0 to 6240;
-    signal pt1_int : natural range 0 to 2558;
-    signal pt2_int : natural range 0 to 2558;
-    signal cosh_deta_int : natural range 0 to 10597282;
-    signal cos_dphi_int : integer range -1000 to 1000;
+    signal eg_eta_integer: diff_integer_inputs_array(0 to 1) := (others => 0);
+    signal eg_phi_integer: diff_integer_inputs_array(0 to 1) := (others => 0);
+    signal diff_eg_eg_eta_integer: dim2_max_eta_range_array(0 to 1, 0 to 1) := (others => (others => 0));
+    signal diff_eg_eg_phi_integer: dim2_max_phi_range_array(0 to 1, 0 to 1) := (others => (others => 0));
+
+    signal pt1_vec : std_logic_vector(EG_PT_VECTOR_WIDTH-1 downto 0);
+    signal pt2_vec : std_logic_vector(EG_PT_VECTOR_WIDTH-1 downto 0);
+    signal cosh_deta_vec : std_logic_vector(EG_EG_COSH_COS_VECTOR_WIDTH-1 downto 0);
+    signal cos_dphi_vec : std_logic_vector(EG_EG_COSH_COS_VECTOR_WIDTH-1 downto 0);
+    signal inv_dr_sq_vec : std_logic_vector(EG_EG_INV_DR_SQ_VECTOR_WIDTH-1 downto 0);
 
 --*********************************Main Body of Code**********************************
 begin
@@ -94,12 +73,9 @@ begin
 
  ------------------- Instantiate  modules  -----------------
 
- -- ****************************************************************************
---HB 2016-11-11: the following calculations of signals should be part of gtl_module.vhd
--- ****************************************************************************
+pt1_vec(EG_PT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR(EG_PT_LUT(CONV_INTEGER(eg_data(0)(D_S_I_EG_V2.et_high downto    D_S_I_EG_V2.et_low))), EG_PT_VECTOR_WIDTH);
 
-pt1_int <= EG_PT_LUT(CONV_INTEGER(eg_data(0)(D_S_I_EG_V2.et_high downto D_S_I_EG_V2.et_low)));
-pt2_int <= EG_PT_LUT(CONV_INTEGER(eg_data(1)(D_S_I_EG_V2.et_high downto D_S_I_EG_V2.et_low)));
+pt2_vec(EG_PT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR(EG_PT_LUT(CONV_INTEGER(eg_data(1)(D_S_I_EG_V2.et_high downto D_S_I_EG_V2.et_low))), EG_PT_VECTOR_WIDTH);
 
 eg_data_l: for i in 0 to 1 generate
     eg_eta_integer(i) <= CONV_INTEGER(signed(eg_data(i)(D_S_I_EG_V2.eta_high downto D_S_I_EG_V2.eta_low)));
@@ -113,24 +89,25 @@ diff_eg_eg_phi_i: entity work.sub_phi_integer_obj_vs_obj
     generic map(2, 2, CALO_PHI_HALF_RANGE_BINS)
     port map(eg_phi_integer, eg_phi_integer, diff_eg_eg_phi_integer);
 
-diff_eta_int <= EG_EG_DIFF_ETA_LUT(diff_eg_eg_eta_integer(0,1));
-diff_phi_int <= EG_EG_DIFF_PHI_LUT(diff_eg_eg_phi_integer(0,1));
+inv_dr_sq_vec <= CONV_STD_LOGIC_VECTOR(EG_EG_INV_DR_SQ_LUT(diff_eg_eg_eta_integer(0,1), diff_eg_eg_phi_integer(0,1)), EG_EG_INV_DR_SQ_VECTOR_WIDTH);
 
-cosh_deta_int <= EG_EG_COSH_DETA_LUT(diff_eg_eg_eta_integer(0,1));
-cos_dphi_int <= EG_EG_COS_DPHI_LUT(diff_eg_eg_phi_integer(0,1));
+cosh_deta_vec <= CONV_STD_LOGIC_VECTOR(EG_EG_COSH_DETA_LUT(diff_eg_eg_eta_integer(0,1)), EG_EG_COSH_COS_VECTOR_WIDTH);
+cos_dphi_vec <= CONV_STD_LOGIC_VECTOR(EG_EG_COS_DPHI_LUT(diff_eg_eg_phi_integer(0,1)), EG_EG_COSH_COS_VECTOR_WIDTH);
 
 dut: entity work.invmass_div_dr_calculator
-    generic map(pt_prec, deta_dphi_prec, cosh_cos_prec,
-        pt_int_digits, deta_int_digits, dphi_int_digits, cosh_deta_int_digits,
-        mass_upper_limit, mass_lower_limit)
+    generic map(
+        mass_upper_limit, mass_lower_limit,
+        EG_PT_VECTOR_WIDTH, EG_PT_VECTOR_WIDTH,
+        EG_EG_COSH_COS_VECTOR_WIDTH, EG_EG_INV_DR_SQ_VECTOR_WIDTH
+    )
     port map(
-        pt1_int, 
-        pt2_int, 
-        diff_eta_int,
-        diff_phi_int,
-        cosh_deta_int, 
-        cos_dphi_int, 
-        open);
+        inv_dr_sq_vec, 
+        pt1_vec, 
+        pt2_vec, 
+        cosh_deta_vec, 
+        cos_dphi_vec, 
+        open, open, open
+    );
 
 end beh;
 

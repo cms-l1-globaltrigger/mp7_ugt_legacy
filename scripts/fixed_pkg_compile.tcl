8b95325b4261c1765053ef6ad140ec31fecbf7fc
herbberg <herbert.bergauer@oeaw.ac.at>
2020-05-15 07:58:06 +0200

    removed unused files

--- scripts/fixed_pkg_compile.tcl
+++
@@ -1,8 +0,0 @@
-open_project top/top.xpr
-add_files -norecurse ../../src/mp7_ugt_legacy/firmware/hdl/fixed_pkg_2008.vhd
-set_property library ieee [get_files ../../src/mp7_ugt_legacy/firmware/hdl/fixed_pkg_2008.vhd]
-read_vhdl -vhdl2008 ./../../src/mp7/boards/mp7/base_fw/mp7xe_690/firmware/hdl/mp7xe_690.vhd
-#launch_runs synth_1 -jobs 4
-#wait_on_run synth_1
-#open_run synth_1 -name synth_1
-close_project

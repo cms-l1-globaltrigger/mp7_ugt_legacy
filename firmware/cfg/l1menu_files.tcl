5bb8df4d0a3207eb646cfafef228bbb7754c2872
herbberg <Herbert.Bergauer@oeaw.ac.at>
2020-10-28 09:10:05 +0100

    updated

--- firmware/cfg/l1menu_files.tcl
+++ firmware/cfg/l1menu_files.tcl
@@ -3,11 +3,11 @@
 set mod_id $env(module_id)
 # puts $mod_id
 
-set gtl_pkg "../../../src/module_${mod_id}/gtl_pkg.vhd"
+set gtl_pkg "../../src/module_${mod_id}/gtl_pkg.vhd"
 # puts $gtl_pkg
-set algo_mapping_rop "../../../src/module_${mod_id}/algo_mapping_rop.vhd"
+set algo_mapping_rop "../../src/module_${mod_id}/algo_mapping_rop.vhd"
 # puts $algo_mapping_rop
-set gtl_module "../../../src/module_${mod_id}/gtl_module.vhd"
+set gtl_module "../../src/module_${mod_id}/gtl_module.vhd"
 # puts $gtl_module
 
 add_files -norecurse -fileset sources_1 $gtl_pkg $algo_mapping_rop $gtl_module

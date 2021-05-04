544994e56fc3b214a17c7d607205fa6aa1e8546b
herbberg <Herbert.Bergauer@oeaw.ac.at>
2021-05-04 12:32:10 +0200

    bug fixed

--- firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/common/tbpt_calc.vhd
+++ firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/common/tbpt_calc.vhd
@@ -41,8 +41,8 @@ architecture rtl of tbpt_calc is
 -- HB 2017-03-23: calculation of twobody_pt with formular => pt**2 = pt1**2+pt2**2+2*pt1*pt2*(cos(phi1)*cos(phi2)+sin(phi1)*sin(phi2))
 -- PT_SQ_VECTOR_WIDTH based on formular for pt**2 [2+... because of ...+2*pt1*pt2*(cos(phi1)*cos(phi2)+sin(phi1)*sin(phi2))]
     constant PT_SQ_VECTOR_WIDTH : positive := 2+pt1_width+pt2_width+sin_cos_width+sin_cos_width;
-    signal pt1_square : std_logic_vector(pt1_width+pt1_width-1 downto 0);
-    signal pt2_square : std_logic_vector(pt2_width+pt2_width-1 downto 0);
+    signal pt1_square : std_logic_vector(PT_SQ_VECTOR_WIDTH-1 downto 0);
+    signal pt2_square : std_logic_vector(PT_SQ_VECTOR_WIDTH-1 downto 0);
 
     signal cos_plus_sin_integer : integer;
     signal cos_plus_sin_vec_temp : std_logic_vector(sin_cos_width+sin_cos_width-1 downto 0);
@@ -51,8 +51,8 @@ architecture rtl of tbpt_calc is
     signal pt1_x_pt2_x_cos_plus_sin : std_logic_vector(PT_SQ_VECTOR_WIDTH-1 downto 0);
 
     constant UPT_SQ_VECTOR_WIDTH : positive := 2+upt1_width+upt2_width+sin_cos_width+sin_cos_width;
-    signal upt1_square : std_logic_vector(upt1_width+upt1_width-1 downto 0);
-    signal upt2_square : std_logic_vector(upt2_width+upt2_width-1 downto 0);
+    signal upt1_square : std_logic_vector(UPT_SQ_VECTOR_WIDTH-1 downto 0);
+    signal upt2_square : std_logic_vector(UPT_SQ_VECTOR_WIDTH-1 downto 0);
 
     signal upt1_x_upt2_x_cos_plus_sin_temp : std_logic_vector(UPT_SQ_VECTOR_WIDTH-1 downto 0);
     signal upt1_x_upt2_x_cos_plus_sin : std_logic_vector(UPT_SQ_VECTOR_WIDTH-1 downto 0);
@@ -73,10 +73,10 @@ begin
 --               pt_square = pt1**2+pt2**2+2*pt1*pt2*cos_plus_sin_vec
 
 -- calculation tbpt
---     pt1_square <= pt1 * pt1 * conv_std_logic_vector(10**(sin_cos_precision*2), PT_SQ_VECTOR_WIDTH-pt1_width*2);
---     pt2_square <= pt2 * pt2 * conv_std_logic_vector(10**(sin_cos_precision*2), PT_SQ_VECTOR_WIDTH-pt2_width*2);
-    pt1_square <= pt1 * pt1;
-    pt2_square <= pt2 * pt2;
+    pt1_square <= pt1 * pt1 * conv_std_logic_vector(10**(sin_cos_precision*2), PT_SQ_VECTOR_WIDTH-pt1_width*2);
+    pt2_square <= pt2 * pt2 * conv_std_logic_vector(10**(sin_cos_precision*2), PT_SQ_VECTOR_WIDTH-pt2_width*2);
+--     pt1_square <= pt1 * pt1;
+--     pt2_square <= pt2 * pt2;
 
     cos_plus_sin_integer <= (cos_phi_1_integer * cos_phi_2_integer) + (sin_phi_1_integer * sin_phi_2_integer);
 
@@ -92,10 +92,10 @@ begin
     tbpt <= pt1_square + pt2_square + pt1_x_pt2_x_cos_plus_sin;
 
 -- calculation tbupt
---     upt1_square <= upt1 * upt1 * conv_std_logic_vector(10**(sin_cos_precision*2), PT_SQ_VECTOR_WIDTH-upt1_width*2);
---     upt2_square <= upt2 * upt2 * conv_std_logic_vector(10**(sin_cos_precision*2), PT_SQ_VECTOR_WIDTH-upt2_width*2);
-    upt1_square <= upt1 * upt1;
-    upt2_square <= upt2 * upt2;
+    upt1_square <= upt1 * upt1 * conv_std_logic_vector(10**(sin_cos_precision*2), UPT_SQ_VECTOR_WIDTH-upt1_width*2);
+    upt2_square <= upt2 * upt2 * conv_std_logic_vector(10**(sin_cos_precision*2), UPT_SQ_VECTOR_WIDTH-upt2_width*2);
+--     upt1_square <= upt1 * upt1;
+--     upt2_square <= upt2 * upt2;
 
     upt1_x_upt2_x_cos_plus_sin_temp <= conv_std_logic_vector(2,2) * upt1 * upt2 * cos_plus_sin_vec;
 -- HB 2017-03-22: use two's complement for pt1_x_pt2_x_cos_plus_sin when cos_plus_sin_vec_temp is negative

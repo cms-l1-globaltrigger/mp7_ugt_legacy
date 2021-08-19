8bb96cb3da798b0692cd2359dcb6e5d816ca5f48
herbberg <herbert.bergauer@oeaw.ac.at>
2021-08-19 11:51:38 +0200

    updated scripts, added new coe files, renamed directory

---
+++ doc/rom_lut_calo_inv_dr_sq/muon_inv_dr_sq_calc.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import sys
+import math
+import os
+
+coe_file_path=os.path.join(os.path.expanduser("~"), "github/cms-l1-globaltrigger/mp7_ugt_legacy/firmware/ngc")
+#print(coe_file_path)
+
+# double bin width
+eta_factor=2
+eta_bin_width=0.087/8*eta_factor
+eta_min_bin=-225
+eta_max_bin=225
+deta_bins=int((abs(eta_min_bin)+eta_max_bin)/eta_factor)+1
+# same bin width as for calos
+phi_factor=4
+phi_bins=576
+phi_bins_reduced=phi_bins/phi_factor
+dphi_bins=int(phi_bins_reduced/2)
+print(phi_bins_reduced, dphi_bins)
+# same precision as for muon invariant mass (2*pt_precision+cosh_cos_precision) [=1+1+4]
+precision=6
+
+undef="undefined"
+inv_dr_sq_fw_lut_arr={}
+
+idx=0
+part=0
+
+print(f"{'idx':>6}", f"{'dphi_idx':>9}", f"{'deta_idx':>9}", f"{'dphi_val':>22}", f"{'deta_val':>22}", f"{'inv_dr_sq':>25}", f"{'inv_dr_sq_rounded':>18}", f"{'inv_dr_sq_fw_lut':>17}")
+
+for dphi_msb in range(0,2):
+    for deta_msb in range(0,4):
+        part+=1
+        filename=os.path.join(coe_file_path, "rom_lut_muon_inv_dr_sq_part" + str(part) + "_new.coe")
+        f = open(filename, "w")
+        print("memory_initialization_radix=10;", file=f)
+        print("memory_initialization_vector=", file=f)
+        for deta_idx in range(0,64):
+            for dphi_idx in range(0,64):
+                deta_idx_gl = deta_idx+deta_msb*64
+                deta_val = deta_idx_gl*eta_bin_width
+                dphi_idx_gl = dphi_idx+dphi_msb*64
+                dphi_val = dphi_idx_gl*2*math.pi/phi_bins_reduced
+                if deta_val == 0 and dphi_val == 0:
+                    inv_dr_sq_fw_lut_arr[0] = 0
+
+                    print(f"{idx:>6}", f"{dphi_idx:>9}", f"{deta_idx:>9}", f"{dphi_val:>22}", f"{deta_val:>22}", f"{undef:>25s}", f"{undef:>18s}", f"{undef:>17s}")
+                else:
+                    inv_dr_sq = 1/((deta_val**2)+(dphi_val**2))
+                    inv_dr_sq_rounded = round(inv_dr_sq,precision)
+                    inv_dr_sq_fw_lut = int(round(inv_dr_sq_rounded*(10**precision),0))
+                    inv_dr_sq_fw_lut_arr[idx] = inv_dr_sq_fw_lut
+
+                    if deta_idx_gl <= 226 and dphi_idx_gl <= 72:
+                        print(f"{idx:>6}", f"{dphi_idx_gl:>9}", f"{deta_idx_gl:>9}", f"{dphi_val:>22}", f"{deta_val:>22}", f"{inv_dr_sq:>25}", f"{inv_dr_sq_rounded:>18}", f"{inv_dr_sq_fw_lut:>17}")
+
+                loc_idx=idx-(part-1)*64*64
+                for i in range(0,4):
+                    j=(i*16)+(deta_idx*64)
+                    if loc_idx >= j and loc_idx < j+15:
+                        print(inv_dr_sq_fw_lut_arr[idx], end = ',', file=f)
+                    if loc_idx == j+15:
+                        print(inv_dr_sq_fw_lut_arr[idx], end = ',\n', file=f)
+
+                idx+=1
+        print(";", file=f)
+        f.close()

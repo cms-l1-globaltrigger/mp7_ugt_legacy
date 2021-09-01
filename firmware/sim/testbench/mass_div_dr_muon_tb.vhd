a5d348894d918745585651da9378e83b0829e03e
herbberg <herbert.bergauer@oeaw.ac.at>
2021-09-01 13:00:53 +0200

    added simulation files (for mass_div_dr_muon)

---
+++ firmware/sim/testbench/mass_div_dr_muon_tb.vhd
@@ -0,0 +1,199 @@
+
+-- Description:
+-- Testbench for simulation of mass div DR
+
+-- Version history:
+-- HB 2020-08-25: first design
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use ieee.fixed_pkg.all;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+library std;                  -- for Printing
+use std.textio.all;
+
+use work.math_pkg.all;
+use work.gtl_pkg.all;
+
+entity mass_div_dr_muon_tb is
+end mass_div_dr_muon_tb;
+
+architecture beh of mass_div_dr_muon_tb is
+
+    signal lhc_clk : std_logic;
+
+    constant LHC_CLK_PERIOD  : time :=  25 ns;
+
+    signal bx_data : bx_data_record;
+
+    signal mu_bx_0_pt_vector: diff_inputs_array(0 to NR_MU_OBJECTS-1) := (others => (others => '0'));
+    signal mu_bx_0_upt_vector: diff_inputs_array(0 to NR_MU_OBJECTS-1) := (others => (others => '0'));
+    signal mu_bx_0_eta_integer: integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_bx_0_phi_integer: integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_bx_0_cos_phi: integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_bx_0_sin_phi: integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+
+    signal ls_charcorr_double_bx_0_bx_0, os_charcorr_double_bx_0_bx_0 : std_logic_2dim_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1);
+    signal ls_charcorr_triple_bx_0_bx_0, os_charcorr_triple_bx_0_bx_0 : std_logic_3dim_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1);
+    signal ls_charcorr_quad_bx_0_bx_0, os_charcorr_quad_bx_0_bx_0 : std_logic_4dim_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1);
+
+    signal mu_mu_bx_0_bx_0_deta_integer: dim2_max_eta_range_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_deta_vector: deta_dphi_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_dphi_integer: dim2_max_phi_range_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_dphi_vector: deta_dphi_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_mass_inv_pt : mass_dim2_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_mass_over_dr : mass_div_dr_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+
+--*********************************Main Body of Code**********************************
+begin
+
+    -- Clock
+    process
+    begin
+        lhc_clk  <=  '1';
+        wait for LHC_CLK_PERIOD/2;
+        lhc_clk  <=  '0';
+        wait for LHC_CLK_PERIOD/2;
+    end process;
+
+    process
+        variable eta_val_max : std_logic_vector(8 downto 0) := '0' & X"E1";
+        variable phi_val_max : std_logic_vector(9 downto 0) := "00" & X"90";
+    begin
+        wait for 5 ns;
+        bx_data.mu(2) <= (others => X"0000000000000000");
+        wait for LHC_CLK_PERIOD;
+        bx_data.mu(2)(0)(18 downto 10) <= '0' & X"15";
+        bx_data.mu(2)(0)(31 downto 23) <= eta_val_max;
+        bx_data.mu(2)(0)(9 downto 0) <= phi_val_max;
+        bx_data.mu(2)(1)(18 downto 10) <= '0' & X"15";
+-- voller Breich für deta und dphi fehlt noch !!??
+        for i in 0 to 226-1 loop
+            bx_data.mu(2)(1)(31 downto 23) <= (eta_val_max - i*2);
+            for j in 0 to 72 loop
+                bx_data.mu(2)(1)(9 downto 0) <= (phi_val_max - j*2);
+                wait for LHC_CLK_PERIOD;
+            end loop;
+        end loop;
+        wait;
+    end process;
+
+ ------------------- Instantiate  modules  -----------------
+
+cond_invariant_mass_delta_r_i1_i: entity work.correlation_conditions
+    generic map(
+-- slices for muon
+        slice_low_obj1 => 0,
+        slice_high_obj1 => 7,
+        slice_low_obj2 => 0,
+        slice_high_obj2 => 7,
+-- obj cuts
+        pt_threshold_obj1 => X"0015",
+        pt_threshold_obj2 => X"0015",
+-- correlation cuts
+        mass_cut => true,
+        mass_type => INVARIANT_MASS_DIV_DR_TYPE,
+        mass_div_dr_vector_width => MU_MU_MASS_DIV_DR_VECTOR_WIDTH,
+        mass_div_dr_threshold => X"000000000000000BEBC20",
+-- number of objects and type
+        nr_obj1 => NR_MU_OBJECTS,
+        type_obj1 => MU_TYPE,
+        nr_obj2 => NR_MU_OBJECTS,
+        type_obj2 => MU_TYPE,
+-- selector same/different bunch crossings
+        same_bx => true
+    )
+    port map(
+        lhc_clk,
+        muon_obj1 => bx_data.mu(2),
+        muon_obj2 => bx_data.mu(2),
+        mass_div_dr => mu_mu_bx_0_bx_0_mass_over_dr,
+        condition_o => open
+    );
+
+calc_obj_parameter_mu_bx_0_i: entity work.obj_parameter
+    generic map(
+        nr_obj => NR_MU_OBJECTS,
+        type_obj => MU_TYPE
+    )
+    port map(
+        muon => bx_data.mu(2),
+        pt_vector => mu_bx_0_pt_vector,
+        upt_vector => mu_bx_0_upt_vector,
+        eta_integer => mu_bx_0_eta_integer,
+        phi_integer => mu_bx_0_phi_integer,
+        cos_phi => mu_bx_0_cos_phi,
+        sin_phi => mu_bx_0_sin_phi
+    );
+
+calc_deta_dphi_integer_mu_mu_bx_0_bx_0_i: entity work.deta_dphi_calculations
+    generic map(
+        phi_half_range => MUON_PHI_HALF_RANGE_BINS,
+        nr_obj1 => NR_MU_OBJECTS,
+        type_obj1 => MU_TYPE,
+        nr_obj2 => NR_MU_OBJECTS,
+        type_obj2 => MU_TYPE
+    )
+    port map(
+        eta_integer_obj1 => mu_bx_0_eta_integer,
+        phi_integer_obj1 => mu_bx_0_phi_integer,
+        eta_integer_obj2 => mu_bx_0_eta_integer,
+        phi_integer_obj2 => mu_bx_0_phi_integer,
+        deta_integer => mu_mu_bx_0_bx_0_deta_integer,
+        dphi_integer => mu_mu_bx_0_bx_0_dphi_integer
+    );
+
+calc_cut_mass_inv_pt_mu_mu_bx_0_bx_0_i: entity work.correlation_cuts_calculation
+    generic map(
+        nr_obj1 => NR_MU_OBJECTS,
+        type_obj1 => MU_TYPE,
+        nr_obj2 => NR_MU_OBJECTS,
+        type_obj2 => MU_TYPE,
+        mass_cut => true,
+        mass_type => INVARIANT_MASS_TYPE,
+        pt1_width => MU_PT_VECTOR_WIDTH,
+        pt2_width => MU_PT_VECTOR_WIDTH,
+        cosh_cos_width => MUON_MUON_COSH_COS_VECTOR_WIDTH
+    )
+    port map(
+        deta_integer => mu_mu_bx_0_bx_0_deta_integer,
+        dphi_integer => mu_mu_bx_0_bx_0_dphi_integer,
+        pt1 => mu_bx_0_pt_vector,
+        pt2 => mu_bx_0_pt_vector,
+        inv_mass_pt => mu_mu_bx_0_bx_0_mass_inv_pt
+    );
+
+calc_cut_mass_over_dr_mu_mu_bx_0_bx_0_i: entity work.correlation_cuts_calculation
+    generic map(
+        nr_obj1 => NR_MU_OBJECTS,
+        type_obj1 => MU_TYPE,
+        nr_obj2 => NR_MU_OBJECTS,
+        type_obj2 => MU_TYPE,
+        pt1_width => MU_PT_VECTOR_WIDTH,
+        pt2_width => MU_PT_VECTOR_WIDTH,
+        cosh_cos_width => MUON_MUON_COSH_COS_VECTOR_WIDTH,
+        mass_over_dr_cut => true,
+        rom_sel => MU_MU_ROM,
+        deta_bins_width => MU_DETA_BINS_WIDTH_ROM,
+        dphi_bins_width => MU_DETA_BINS_WIDTH_ROM,
+        inverted_dr_sq_width => MU_MU_INV_DR_SQ_VECTOR_WIDTH
+    )
+    port map(
+        lhc_clk,
+        deta_integer => mu_mu_bx_0_bx_0_deta_integer,
+        dphi_integer => mu_mu_bx_0_bx_0_dphi_integer,
+        inv_mass_pt_in => mu_mu_bx_0_bx_0_mass_inv_pt,
+        mass_over_dr => mu_mu_bx_0_bx_0_mass_over_dr
+    );
+
+calc_muon_charge_correlations_bx_0_bx_0_i: entity work.muon_charge_correlations
+    port map(bx_data.mu(2), bx_data.mu(2),
+        ls_charcorr_double_bx_0_bx_0, os_charcorr_double_bx_0_bx_0,
+        ls_charcorr_triple_bx_0_bx_0, os_charcorr_triple_bx_0_bx_0,
+        ls_charcorr_quad_bx_0_bx_0, os_charcorr_quad_bx_0_bx_0);
+
+end beh;
+

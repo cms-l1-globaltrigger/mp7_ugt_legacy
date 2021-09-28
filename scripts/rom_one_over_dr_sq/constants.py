5f91e838fa2f6a2b2b3baad87c20579f5a9c9894
herbberg <herbert.bergauer@oeaw.ac.at>
2021-09-28 11:56:52 +0200

    added comments

--- scripts/rom_one_over_dr_sq/constants.py
+++ scripts/rom_one_over_dr_sq/constants.py
@@ -1,46 +1,50 @@
-"""Constants for scripts of 1/DR^2.
+"""Constants for scripts of 1/DR^2 calculations, LUTs and checks.
 """
 
 # BRAM
-bram_size = 18432
+bram_size = 18432 # BRAM size of Virtex chip
 
 # calos
-calo_objects = 12
+calo_objects = 12 # see interface definition (scales_inputs_2_ugt.pdf)
 
+## original bin width for eta
 calo_eta_bin_width = 0.087/2 # see interface definition (scales_inputs_2_ugt.pdf)
 calo_eta_min_bin = -115 # see interface definition (scales_inputs_2_ugt.pdf)
 calo_eta_max_bin = 114 # see interface definition (scales_inputs_2_ugt.pdf)
 calo_eta_bins = abs(calo_eta_min_bin)+(calo_eta_max_bin+1) # "+1" is for eta bin 0 (0..0.087/2)
+## original bin width for phi
 calo_phi_bins = 144 # see interface definition (scales_inputs_2_ugt.pdf)
 
-calo_precision = 5 # see scales in menu XML (e.g.: <object>PRECISION</object> <type>EG-EG-InverseDeltaRMath</type> <n_bits>5</n_bits>)
+calo_precision = 5 # value from scales in menu XML (<object>PRECISION</object> <type>EG-EG-InverseDeltaRMath</type> <n_bits>5</n_bits>) [same value for all calo object types]
+
+## definitions for calo ROMs (containing LUTs for 1/DR^2)
 calo_rom_size = 4096
 calo_nr_roms = 5
-
 calo_deta_block_size_rom1_4 = 64
 calo_dphi_block_size_rom1_4 = 64
 calo_deta_block_size_rom5 = 256
 calo_dphi_block_size_rom5 = 16
 
 # muon
-muon_objects = 8
+muon_objects = 8 # see interface definition (scales_inputs_2_ugt.pdf)
 
-## double bin width for eta
+## double bin width for eta (depends on resources of BRAMs in Virtex chip)
 muon_eta_bin_width_def = 0.087/8 # see interface definition (scales_inputs_2_ugt.pdf)
 muon_eta_factor = 2 # double bin width
 muon_eta_bin_width = muon_eta_bin_width_def*muon_eta_factor
 muon_eta_min_bin = -225 # see interface definition (scales_inputs_2_ugt.pdf)
 muon_eta_max_bin = 225 # see interface definition (scales_inputs_2_ugt.pdf)
 muon_eta_bins = int((abs(muon_eta_min_bin)+muon_eta_max_bin)/muon_eta_factor)+1
-## double bin width for phi
+## double bin width for phi (depends on resources of BRAMs in Virtex chip)
 muon_phi_bins = 576 # see interface definition (scales_inputs_2_ugt.pdf)
 muon_phi_factor = 2 # double bin width
-muon_phi_bins_reduced = int(muon_phi_bins/muon_phi_factor)
+muon_phi_bins_reduced = int(muon_phi_bins/muon_phi_factor) # double bin width
 
-muon_precision = 5 # see scales in menu XML (<object>PRECISION</object> <type>MU-MU-InverseDeltaRMath</type> <n_bits>5</n_bits>)
+muon_precision = 5 # value from scales in menu XML (<object>PRECISION</object> <type>MU-MU-InverseDeltaRMath</type> <n_bits>5</n_bits>)
+
+## definitions for muon ROMs (containing LUTs for 1/DR^2)
 muon_rom_size = 8192
 muon_nr_roms = 6
-
 muon_deta_block_size = 128
 muon_dphi_block_size = 64
 

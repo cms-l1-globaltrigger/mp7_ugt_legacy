56d5fe0e8f7163b88849a619c6f8da05cf454e49
herbberg <herbert.bergauer@oeaw.ac.at>
2022-03-23 10:48:32 +0100

    updated framework.tex

--- doc/mp7_ugt_firmware_specification/src/latex/interfaces/spytrig.vhd
+++ doc/mp7_ugt_firmware_specification/src/latex/interfaces/spytrig.vhd
@@ -1,18 +1,13 @@
 entity spytrig is
-	port
-	(
-		lhc_clk    : in  std_logic;
-		lhc_rst    : in  std_logic;
-		orbit_nr   : in  orbit_nr_t;
-		bx_nr      : in bx_nr_t;
-		sw_reg_i   : in  sw_reg_spytrigger_in_t;
-		sw_reg_o   : out sw_reg_spytrigger_out_t;
-
-		spy1_o     : out std_logic;
-		spy2_o     : out std_logic;
-		spy3_o     : out std_logic;
-		spy3_ack_i : in  std_logic;
-
-		simmem_in_use_i : in std_logic
-	);
+    port
+    (
+        lhc_clk    : in  std_logic;
+        lhc_rst    : in  std_logic;
+        orbit_nr   : in  orbit_nr_t;
+        bx_nr      : in bx_nr_t;
+        sw_reg_i   : in  sw_reg_spytrigger_in_t;
+        sw_reg_o   : out sw_reg_spytrigger_out_t;
+        spy1_o     : out std_logic;
+        spy2_o     : out std_logic
+    );
 end;

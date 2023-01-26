1b59fbce31797c9b6616b2f80753b9c49f2c70e6
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-01-26 15:34:24 +0100

    updated script

--- firmware/sim/scripts/run_adt_test_l1menu_adt_v6.py
+++ firmware/sim/scripts/run_adt_test_l1menu_adt_v6.py
@@ -1,5 +1,20 @@
+import argparse
 import os
 import logging
+
+TEST_DIR_DEF = "adt_test"
+L1MENU_DEF = "l1menu_adt_v6"
+
+PKG_NAME = "adt_test_sim_pkg"
+PKG_TPL = "{}_tpl.vhd".format(PKG_NAME)
+PKG_TMP = "{}_tmp.vhd".format(PKG_NAME)
+PKG_FILE = "{}.vhd".format(PKG_NAME)
+DO_NAME = "adt_test_l1menu_adt_v6"
+DO_TPL = "{}_tpl.do".format(DO_NAME)
+DO_FILE = "{}.do".format(DO_NAME)
+
+# order in algo_bit_name: module 0 to 3 (key = algo bit, value = algo name)
+algo_bit_name = {0: "L1_ADT_20000", 2: "L1_ADT_400", 1: "L1_ADT_4000", 3: "L1_ADT_80"}
 
 def run_command(*args):
     command = ' '.join(args)
@@ -19,34 +34,44 @@ def render_template(src, dst, args):
     with open(dst, 'w') as dst:
         dst.write(content)
 
-def main():
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--menu', default=L1MENU_DEF, help="menu name of L1Menu for adt test (default is '{}')".format(L1MENU_DEF))
+    parser.add_argument('--test_dir', default=TEST_DIR_DEF, help="dir name for adt test data (default is '{}')".format(TEST_DIR_DEF))
+    #parser.add_argument('--algo_bits', help="algo bits (in module order)")
+    #parser.add_argument('--algo_names', help="algo names (in module order)")
+    return parser.parse_args()
 
-    # order in algo_bit_name: module 0 to 3 (key = algo bit, value = algo name)
-    algo_bit_name = {0:"L1_ADT_20000", 2:"L1_ADT_400", 1:"L1_ADT_4000", 3:"L1_ADT_80"}
+def main():
+    args = parse_args()
+
     err_files = {}
     
     work_dir = os.path.abspath(os.getcwd())
+    
+    TEST_DIR = args.test_dir
+    L1MENU = args.menu
         
     for i in range(len(algo_bit_name)):
         module_nr = 'module_{}'.format(i)
-        error_file_path = os.path.join(work_dir, 'adt_test', 'l1menu_adt_v6', module_nr, 'error_file_{}.txt').format(algo_bit_name.get(list(algo_bit_name.keys())[i]))
+        error_file_path = os.path.join(work_dir, TEST_DIR, L1MENU, module_nr, 'error_file_{}.txt').format(algo_bit_name.get(list(algo_bit_name.keys())[i]))
         err_files[i] = error_file_path
         
-        vhdl_file_tpl_path = os.path.join(work_dir, 'testbench', 'templates', 'adt_test_sim_pkg_tpl.vhd')
-        vhdl_file_path = os.path.join(work_dir, 'testbench', 'adt_test_sim_pkg_tmp.vhd')
+        vhdl_file_tpl_path = os.path.join(work_dir, 'testbench', 'templates', PKG_TPL)
+        vhdl_file_path = os.path.join(work_dir, 'testbench', PKG_TMP)
         render_template(vhdl_file_tpl_path, vhdl_file_path, {'{{ALGO_BIT}}': str(list(algo_bit_name.keys())[i])})
         
-        vhdl_file_tpl_path = os.path.join(work_dir, 'testbench', 'adt_test_sim_pkg_tmp.vhd')
-        vhdl_file_path = os.path.join(work_dir, 'testbench', 'adt_test_sim_pkg.vhd')
+        vhdl_file_tpl_path = os.path.join(work_dir, 'testbench', PKG_TMP)
+        vhdl_file_path = os.path.join(work_dir, 'testbench', PKG_FILE)
         render_template(vhdl_file_tpl_path, vhdl_file_path, {'{{ERR_FILE_LOC}}': error_file_path})
         
-        do_file_tpl_path = os.path.join(work_dir, 'scripts', 'templates', 'adt_test_l1menu_adt_v6_tpl.do')
-        do_file_path = os.path.join(work_dir, 'scripts', 'adt_test_l1menu_adt_v6.do')
+        do_file_tpl_path = os.path.join(work_dir, 'scripts', 'templates', DO_TPL)
+        do_file_path = os.path.join(work_dir, 'scripts', DO_FILE)
         render_template(do_file_tpl_path, do_file_path, {'{{MOD_NR}}': module_nr})
         
-        do_cmds_path = os.path.join(work_dir, 'adt_test', 'l1menu_adt_v6', module_nr, 'temp.txt')
+        do_cmds_path = os.path.join(work_dir, TEST_DIR, L1MENU, module_nr, 'temp.txt')
         exec_do_cmds = ". " + do_cmds_path
-        line = '/opt/mentor/questasim/bin/vsim -c -do "do scripts/adt_test_l1menu_adt_v6.do; quit -f"'
+        line = '/opt/mentor/questasim/bin/vsim -c -do "do scripts/{}; quit -f"'.format(DO_FILE)
         with open(do_cmds_path, 'w') as f:
             f.write(line)
             f.write('\n')
@@ -58,7 +83,7 @@ def main():
         if os.path.getsize(err_files[i]) != 0:
             print('Errors in file: {}'.format(err_files[i]))
 
-        command = f'bash -c "rm {work_dir}/adt_test/l1menu_adt_v6/module_{i}/temp.txt"'
+        command = f'bash -c "rm {work_dir}/{TEST_DIR}/{L1MENU}/module_{i}/temp.txt"'
         run_command(command)
         
     command = f'bash -c "rm {vhdl_file_path}; rm {do_file_path}"'

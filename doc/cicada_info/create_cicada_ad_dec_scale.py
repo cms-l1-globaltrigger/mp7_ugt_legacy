83480502d784136a1147df79d364832a9d37fec3
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2023-11-16 12:04:49 +0100

    added cicada_info

---
+++ doc/cicada_info/create_cicada_ad_dec_scale.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+n_bits = 8
+steps = 2**n_bits
+scale_step = 1/steps
+max_val = (scale_step*steps)-scale_step
+
+print("    <scale>")
+print("      <object>CICADAADDEC</object>")
+print("      <type>COUNT</type>")
+print("      <minimum>+0.0000000000000000E+00</minimum>")
+print("      <maximum>+{:.16E}</maximum>".format(max_val))
+print("      <step>+{:.16E}</step>".format(scale_step))
+print("      <n_bits>{}</n_bits>".format(n_bits))
+for i in range(0, steps):
+    dec_val = scale_step * i
+    print("      <bin>")
+    print("        <number>{}</number>".format(i))
+    print("        <minimum>+{:.16E}</minimum>".format(dec_val))
+    print("        <maximum>+{:.16E}</maximum>".format(dec_val))
+    print("      </bin>")
+print("    </scale>")

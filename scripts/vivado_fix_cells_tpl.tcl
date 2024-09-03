664562b2380bfbefac4ec91c4f0b0ad61a7521f3
Herbert Bergauer <hbergaue@srv-b1b07-10-01.cern.ch>
2024-09-03 10:52:08 +0200

    added scripts

---
+++ scripts/vivado_fix_cells_tpl.tcl
@@ -0,0 +1,9 @@
+open_project {path}/proj/module_{module}/module_{module}/module_{module}.xpr
+open_run synth_1 -name synth_1
+source {path}/src/constraints_fixed_cells.tcl
+file mkdir {path}/proj/module_{module}/module_{module}/module_{module}.srcs/constrs_1/new
+close [ open {path}/proj/module_{module}/module_{module}/module_{module}.srcs/constrs_1/new/fixed_cells.xdc w ]
+add_files -fileset constrs_1 {path}/proj/module_{module}/module_{module}/module_{module}.srcs/constrs_1/new/fixed_cells.xdc
+set_property target_constrs_file {path}/proj/module_{module}/module_{module}/module_{module}.srcs/constrs_1/new/fixed_cells.xdc [current_fileset -constrset]
+save_constraints -force
+close_project

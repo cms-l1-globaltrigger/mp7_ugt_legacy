4030df2b516b3ecd388d62107673bb2cdef46b46
herbberg <herbert.bergauer@oeaw.ac.at>
2024-09-04 16:49:36 +0200

    added tcl script

---
+++ scripts/vivado_fix_cells.tcl
@@ -0,0 +1,11 @@
+set proj_path [lindex $argv 0]
+set module_nr [lindex $argv 1]
+open_project $proj_path/proj/module_$module_nr/module_$module_nr/module_$module_nr.xpr
+open_run synth_1 -name synth_1
+source $proj_path/src/mp7_ugt_legacy/scripts/constraints_fixed_cells.tcl
+file mkdir $proj_path/proj/module_$module_nr/module_$module_nr/module_$module_nr.srcs/constrs_1/new
+close [ open $proj_path/proj/module_$module_nr/module_$module_nr/module_$module_nr.srcs/constrs_1/new/fixed_cells.xdc w ]
+add_files -fileset constrs_1 $proj_path/proj/module_$module_nr/module_$module_nr/module_$module_nr.srcs/constrs_1/new/fixed_cells.xdc
+set_property target_constrs_file $proj_path/proj/module_$module_nr/module_$module_nr/module_$module_nr.srcs/constrs_1/new/fixed_cells.xdc [current_fileset -constrset]
+save_constraints -force
+close_project
